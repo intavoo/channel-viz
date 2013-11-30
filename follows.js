57860e2508955a093cc8be9b9a5de6ecbbfae8a7
intavoo <greg@intavoo.com>
2013-11-30 16:48:25 +1300

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= 'H8OMTDPF9KZhlARyZrhcXhl6hHoDdsCRqjq2EATHw1eTyoOi', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [443398514], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Demo Visualisation', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
